2d59755b13552f24061cba92b19d8b637196120f
fangcong <1186977171@qq.com>
2020-01-31 16:23:59 +0800

    project init commit

---
+++ src/main/java/com/mmall/dao/CartMapper.java
@@ -0,0 +1,16 @@
+package com.mmall.dao;
+
+import com.mmall.pojo.Cart;
+import java.util.List;
+
+public interface CartMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Cart record);
+
+    Cart selectByPrimaryKey(Integer id);
+
+    List<Cart> selectAll();
+
+    int updateByPrimaryKey(Cart record);
+}
---
+++ src/main/java/com/mmall/dao/CategoryMapper.java
@@ -0,0 +1,16 @@
+package com.mmall.dao;
+
+import com.mmall.pojo.Category;
+import java.util.List;
+
+public interface CategoryMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Category record);
+
+    Category selectByPrimaryKey(Integer id);
+
+    List<Category> selectAll();
+
+    int updateByPrimaryKey(Category record);
+}
